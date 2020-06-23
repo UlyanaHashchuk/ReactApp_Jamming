2320b2b0c13471133474858dff1e332c5300fe1a
UlyanaHashchuk <ulyanahashchuk@gmail.com>
2020-06-23 16:20:01 +0200

    jamming react app by classes

---
+++ src/Components/PlayList/PlayList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import './PlayList.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={'Playlist Name'}
+          onChange={this.handleNameChange}
+        />
+        <TrackList tracks={this.props.playlistTracks} 
+          onRemove={this.props.onRemove}
+          isRemoval={true}
+        />
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
+
